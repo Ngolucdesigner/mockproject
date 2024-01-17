354d8ec703c743974059f4ee60550f22b3f14796
Ngolucdesigner <88641960+Ngolucdesigner@users.noreply.github.com>
2024-01-17 09:09:09 +0700

    Delete fe/ecommerce directory

--- fe/ecommerce/src/model/FormatVND.ts
+++
@@ -1,8 +0,0 @@
-const config = {
-    style: "currency",
-    currency: "VND",
-    maximumFractionDigits: 9,
-};
-export const priceFormat = (price:number)=>{
-   return new Intl.NumberFormat("it-IT", config).format(Number(price));
-}
--- fe/ecommerce/src/redux/rootReducer.ts
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux'
-import cartSlice from './slices/cartSlice'
-export const rootReducer = combineReducers({
-   cart: cartSlice,
-    // trong trường hợp có nhiều reducer thì bạn cho vào đây
-  })
-  export type TReducers = ReturnType<typeof rootReducer>
